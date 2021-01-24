ca5331aae7d3ab6417900dfe1e773aeabffbd3e9
CosmicTiger <luisangelmarcia@gmail.com>
2021-01-24 00:11:55 -0600

    Done chapter 08 functions

--- 08_functions.php
+++ 08_functions.php
@@ -1,11 +1,49 @@
 <?php
 
 // Function which prints "Hello I am Zura"
+// function hello() {
+//     echo "Hello I am Luisangel";
+// }
+
+// hello();
+// hello();
+// hello();
 
 // Function with argument
+function hello($name) {
+    return "Hello I am $name";
+}
+
+echo hello('Luisangel').'<br>';
+echo hello('Reyna').'<br>';
 
 // Create sum of two functions
+// function sum($a, $b) {
+//     return $a + $b;
+// }
+
+// echo sum(4, 5);
 
 // Create function to sum all numbers using ...$nums
+// function sum(...$nums) {
+//     $sumatoryNumbers = $nums;
+//     $sumatory = 0;
+
+//     foreach($sumatoryNumbers as $sumatoryNumber ) {
+//         $sumatory += $sumatoryNumber;
+//     }
+
+//     return $sumatory;
+// }
+
+// echo sum(1, 2, 3, 4, 5, 6);
 
 // Arrow functions
+function sum(...$nums) {
+    $sumatoryNumbers = $nums;
+    $sumatory = 0;
+
+    return array_reduce($sumatoryNumbers, fn($sum, $sumatoryNumber) => $sum + $sumatoryNumber);
+}
+
+echo sum(1, 2, 3, 4, 5, 6);
