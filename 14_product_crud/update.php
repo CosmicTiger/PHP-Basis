e337e663d379177879aa4b85f68a0c0616353ac9
CosmicTiger <luisangelmarcia@gmail.com>
2021-02-05 00:58:37 -0600

    Done chapter 21 Update Product

---
+++ 14_product_crud/update.php
@@ -0,0 +1,149 @@
+<?php
+
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', 'C@ncer160799');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$id = $_GET['id'] ?? null;
+
+if (!$id) {
+    header('Location: index.php');
+    exit;
+}
+
+$statement = $pdo->prepare('SELECT * FROM products WHERE id = :id');
+$statement->bindValue(':id', $id);
+$statement->execute();
+$product = $statement->fetch(PDO::FETCH_ASSOC);
+
+// echo '<pre>';
+// var_dump($product);
+// echo '</pre>';
+// exit;
+
+$errors = [];
+
+$title = $product['title'];
+$description = $product['description'];
+$price = $product['price'];
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $title = $_POST['title']; // test
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+    $date = date('Y-m-d H:i:s');
+
+    if (!$title) {
+        $errors[] = 'Product title is required';
+    }
+
+    if (!$price) {
+        $errors[] = 'Product price is required';
+    }
+
+    if (!is_dir('images')) {
+        mkdir('images');
+    }
+
+    if (empty($errors)) {
+        $image = $_FILES['image'] ?? null;
+        $imagePath = $product['image'];
+
+        if ($image && $image['tmp_name']) {
+
+            if ($product['image']) {
+                unlink($product['image']);
+            }
+
+            $imagePath = 'images/' . randomString(8) . '/' . $image['name'];
+            mkdir(dirname($imagePath));
+            move_uploaded_file($image['tmp_name'], $imagePath);
+        }
+
+        $statement = $pdo->prepare("UPDATE products SET title = :title, 
+                    image = :image, description = :description, 
+                    price = :price WHERE id = :id");
+
+        $statement->bindValue(':title', $title);
+        $statement->bindValue(':image', $imagePath);
+        $statement->bindValue(':description', $description);
+        $statement->bindValue(':price', $price);
+        $statement->bindValue(':id', $id);
+        $statement->execute();
+        header('Location: index.php');
+    }
+}
+
+function randomString($n)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $str = '';
+    for ($i = 0; $i < $n; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $str .= $characters[$index];
+    }
+
+    return $str;
+}
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+    <title>Products CRUD</title>
+</head>
+
+<body>
+    <div>
+        <a href="index.php" class="btn btn-secondary">Go back to Products</a>
+    </div>
+
+    <h1>Update Product <?php echo $title ?></h1>
+
+    <?php if (!empty($errors)) : ?>
+        <div class="alert alert-danger">
+            <?php foreach ($errors as $error) : ?>
+                <div><?php echo $error ?></div>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
+
+    <form action="" method="post" enctype="multipart/form-data">
+
+        <?php if ($product['image']) : ?>
+            <img src="<?php echo $product['image'] ?>" class="update-image" />
+        <?php endif; ?>
+
+        <div class="form-group">
+            <label>Product Image</label> <br />
+            <input type="file" name="image">
+        </div>
+        <div class="form-group">
+            <label>Product Title</label>
+            <input type="text" name="title" class="form-control" value="<?php echo $title ?>">
+        </div>
+        <div class="form-group">
+            <label>Product Description</label>
+            <textarea class="form-control" name="description"><?php echo $description ?></textarea>
+        </div>
+        <div class="form-group">
+            <label>Product Price</label>
+            <input type="number" step=".01" name="price" class="form-control" value="<?php echo $price ?>">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
